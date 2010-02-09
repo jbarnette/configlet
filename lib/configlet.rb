4c8cd04bf6be43bac8d4ce937aa0d5fe0908f3f3
John Barnette <jbarnette@gmail.com>
2010-02-09 00:41:08 -0800

    First.

---
+++ lib/configlet.rb
@@ -0,0 +1,85 @@
+# An embarassingly simple environment configuration hash. Too little
+# code to be its own library, really. Inflexible and only good for
+# people who name environment variables exactly like I do.
+
+module Configlet
+
+  # What's at the front of our environment variables? This will be
+  # upcased and a trailing semicolon will be added, so
+  # <tt>Configlet[:foo]</tt> with a prefix of <tt>thunk</tt> maps to
+  # the <tt>THUNK_FOO</tt> environment variable. Default is +nil+.
+
+  attr_accessor :prefix
+
+  # Duh.
+
+  VERSION = "1.0.0"
+
+  I = lambda { |v| v } #:nodoc:
+
+  # Grab a config value. +key+ is translated to an unfriendly
+  # environment name by upcasing, replacing all periods with
+  # underscores, and prepending (with an underscore) the +prefix+.
+  #
+  # If the prefix is set to <tt>"thunk"</tt>, for example, calling
+  # <tt>Configlet[:severity]</tt> will return the value of the
+  # <tt>THUNK_SEVERITY</tt> environment variable, or
+  # <tt>defaults["severity"]</tt> if the env var isn't set.
+
+  def [] key
+    mungers[key.to_s].call ENV[envify key] || defaults[key.to_s]
+  end
+
+  # Set an environment value. +key+ is translated to an unfriendly
+  # environment name as in Configlet#[] above.
+
+  def []= key, value
+    ENV[envify key] = value
+  end
+
+  # Set one or more default values. Use "friendly" names, not env
+  # vars, so a default for the <tt>THUNK_SECRET</tt> could be set
+  # as <tt>Configlet.default :secret => "sssssh"</tt> (assuming a
+  # <tt>"thunk"</tt> prefix).
+
+  def default hash
+    hash.each { |k, v| defaults[k.to_s] = v }
+  end
+
+  def defaults #:nodoc:
+    @defaults ||= {}
+  end
+
+  def envify key #:nodoc:
+    [prefix, key].compact.join("_").tr(".", "_").upcase
+  end
+
+  # Mess with a value when it's retrieved. Useful for turning untyped
+  # environment strings into numbers, booleans, enums, or class
+  # instances. Here's how to munge a boolean:
+  #
+  #    Configlet.prefix = :thunk
+  #    Configlet.munge(:enabled) { |v| "true" == v }
+  #
+  #    ENV["THUNK_ENABLED"] = "false"
+  #    Configlet[:enabled] # => false
+
+  def munge key, &block
+    mungers[key.to_s] = block
+  end
+
+  def mungers #:nodoc:
+    @mungers ||= Hash.new { |h, k| I }
+  end
+
+  # Set prefix to +nil+, clear defaults and mungers.
+
+  def reset
+    self.prefix = nil
+
+    defaults.clear
+    mungers.clear
+  end
+
+  extend self
+end
---
+++ test/test_configlet.rb
@@ -0,0 +1,45 @@
+require "minitest/autorun"
+require "configlet"
+
+class TestConfiglet < MiniTest::Unit::TestCase
+  def setup
+    @env = ENV.to_hash
+    Configlet.reset
+  end
+
+  def teardown
+    ENV.replace @env
+  end
+
+  def test_default
+    assert_nil Configlet["foo"]
+
+    Configlet.default :foo => "bar"
+    assert_equal "bar", Configlet["foo"]
+  end
+
+  def test_get
+    ENV["FOO"] = "bar"
+    assert_equal "bar", Configlet[:foo]
+
+    Configlet.prefix = "boom"
+    ENV["BOOM_FOO"] = "bar"
+    assert_equal "bar", Configlet[:foo]
+  end
+
+  def test_munge
+    ENV["STUPID"] = "true"
+    Configlet.munge(:stupid) { |v| "true" == v }
+    assert_equal true, Configlet[:stupid]
+  end
+
+  def test_set
+    assert_nil ENV["FOO"]
+    Configlet[:foo] = "bar"
+    assert_equal "bar", ENV["FOO"]
+
+    Configlet.prefix = "pow"
+    Configlet[:thud] = "zap"
+    assert_equal "zap", ENV["POW_THUD"]
+  end
+end
